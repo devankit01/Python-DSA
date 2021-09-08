197467459d22fb202aab90f0ec4c617e00a4e5da
Ankit Gupta <58206597+devankit01@users.noreply.github.com>
2021-09-08 21:13:40 +0530

    Update Linear.py

--- Searching/Linear.py
+++ Searching/Linear.py
@@ -1,5 +1,5 @@
 ''' Linear or Sequential Search '''
-
+# Method 1
 searchList = [1,4,6,8,4,3]
 find = int(input())
 for i in searchList:
@@ -8,3 +8,13 @@ for i in searchList:
         break
 else:
     print('Not Found')
+    
+ 
+# Method 2
+pos = 0
+while pos < len(searchList):
+    if searchList[pos] == find:
+        print(pos)
+    pos +=1
+print("-1")
+    
